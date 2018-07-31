9f97a5a0be32086262a11568b813cea939813f21
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-31 07:39:58 +0500

    pusing commits

---
+++ Ch6-P2-Employees/Employee.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P4_EmployeeApp
+{
+    partial class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        private string empSSN; // Read Only
+
+        #region Constructors
+
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay) : this(name, 0, id, pay){}
+        public Employee(string name, int age, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            empAge = age;
+            currPay = pay;
+        }
+
+        public Employee(string name, int age, int id, float pay, string ssn)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+            // OOPS! This is no longer possible if the property is read only.
+            //SocialSecurityNumber = ssn;
+        }
+
+        #endregion
+
+        #region Properties
+
+
+        // Properties!
+        public int Age
+        {
+            get { return empAge; }
+            set => empAge = value;
+        }
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
+        }
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get { return empSSN; }
+        }
+
+        #endregion
+
+        // Accessor (get method).
+        public string GetName()
+        {
+            return empName;
+        }
+        // Mutator (set method).
+        public void SetName(string name)
+        {
+            // Do a check on incoming value
+            // before making assignment.
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name length exceeds 15 characters!");
+            else
+                empName = name;
+        }
+
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            Pay += amount;
+            //currPay += amount; // Not recommended
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Age: {0}", empAge);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}
