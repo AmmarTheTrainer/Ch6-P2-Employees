006902ac9b9377186062b1798df2ed0c17bfd6d4
Ammar Innowi <ammar.shaukat@innowi.com>
2018-08-02 16:04:20 +0500

    3rd commit from head office

---
+++ Ch6-P2-Employees/BenefitPackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_P2_Employees
+{
+    class BenefitPackage
+    {
+
+    }
+}
--- Ch6-P2-Employees/SalesPerson.cs
+++ Ch6-P2-Employees/SalesPerson.cs
@@ -31,4 +31,18 @@ namespace Ch6_P2_Employees
         }
 
     }
+
+    sealed class PTSalesPerson : SalesPerson
+    {
+        public PTSalesPerson(string fullName, int age, int empID, float currPay, string ssn, int numbOfSales) : base(fullName, age, empID, currPay, ssn, numbOfSales)
+        {
+            Console.WriteLine(" 6 agrs ctor Part Time sales person ");
+        }
+        // Assume other members here...
+    }
+
+    //public class PTPTSalesPerson : PTSalesPerson
+    //{
+
+    //}
 }
